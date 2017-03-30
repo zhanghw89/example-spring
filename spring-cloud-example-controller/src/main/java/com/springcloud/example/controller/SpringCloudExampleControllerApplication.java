670a14f2d0a3432658035002dd4001b1bb77ca73
云公司-张会文 <zhanghw@chinatelecom.cn>
2017-03-30 22:50:52 +0800

    init the project

---
+++ spring-cloud-example-controller/src/main/java/com/springcloud/example/controller/SpringCloudExampleControllerApplication.java
@@ -0,0 +1,12 @@
+package com.springcloud.example.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudExampleControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudExampleControllerApplication.class, args);
+	}
+}
