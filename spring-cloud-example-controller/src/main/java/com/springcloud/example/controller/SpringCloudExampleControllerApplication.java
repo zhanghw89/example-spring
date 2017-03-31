7e6cb328a835e5a94a80f0e5a9ad436fd52f244a
云公司-张会文 <zhanghw@chinatelecom.cn>
2017-03-31 22:01:35 +0800

    run demo fix

--- spring-cloud-example-controller/src/main/java/com/springcloud/example/controller/SpringCloudExampleControllerApplication.java
+++ spring-cloud-example-controller/src/main/java/com/springcloud/example/controller/SpringCloudExampleControllerApplication.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -13,6 +15,11 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableCircuitBreaker
 @EnableZuulProxy
 public class SpringCloudExampleControllerApplication {
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudExampleControllerApplication.class, args);
 	}
